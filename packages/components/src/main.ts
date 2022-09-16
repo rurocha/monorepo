31a38edbe8c7b11fa68d0014995ac3bf44b6362f
Ruan Rocha <ruanrocha.iblue@ciclic.com.br>
2022-09-16 14:21:44 -0300

    export

--- packages/components/src/main.ts
+++ packages/components/src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// import { createApp } from 'vue'
+// import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+export { default as Button } from './components/Button.vue'
+export { toUp } from './utils'
